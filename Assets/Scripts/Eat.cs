a2a749d1ca07ba74e6781ad775d570fa1916a950
tyrelh <krankyjoe@gmail.com>
2020-06-21 12:02:28 -0700

    player can eat food and get larger

---
+++ Assets/Scripts/Eat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eat : MonoBehaviour
+{
+    public string Tag;
+    public float increase;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == Tag)
+        {
+            transform.localScale += new Vector3(increase, increase, increase);
+            Destroy(other.gameObject);
+        }
+    }
+}
