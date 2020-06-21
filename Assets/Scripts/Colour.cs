1262188c7bb0a469270764c79ccb391ea9093eef
tyrelh <krankyjoe@gmail.com>
2020-06-21 11:28:03 -0700

    add a random color picker script

---
+++ Assets/Scripts/Colour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colour : MonoBehaviour
+{
+    public List<Material> mats = new List<Material>();
+
+
+    private void Awake()
+    {
+        GetComponent<Renderer>().material = mats[Random.Range(0, mats.Count)];
+    }
+}
