bd3feda7a03fd9c11d130bdcbe7071e20f997ab7
tyrelh <krankyjoe@gmail.com>
2020-06-21 11:03:46 -0700

    add a player and mouse-follow playerController script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float speed;
+    public float sizeSpeedMultiplier;
+
+
+    // Start is called before the first frame update
+    void Start() { }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        target.z = transform.position.z;
+
+        //float movementSpeed = speed * Time.deltaTime * (transform.localScale.x * sizeSpeedMultiplier);
+        float movementSpeed = speed * Time.deltaTime;
+
+        transform.position = Vector3.MoveTowards(transform.position, target, movementSpeed);
+    }
+}
